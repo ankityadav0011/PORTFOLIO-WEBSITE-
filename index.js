b490a34e4541fb9e6a2db8ee6deefdd964b60c66
Ankit Kumar <144920510+ankityadav0011@users.noreply.github.com>
2024-07-28 14:06:41 +0530

    index.js uploaded

---
+++ index.js
@@ -0,0 +1,14 @@
+let tablinks = document.getElementsByClassName("tab-links")
+let tabcontents = document.getElementsByClassName("tab-contents");
+
+function opentab(tabname){
+    for(let  tablink of tablinks){
+            tablink.classList.remove("active-links");    
+    }
+    for(let tabcontent of tabcontents){
+        tabcontent.classList.remove("active-tab"); 
+    }
+     event.currentTarget.classList.add("active-links");
+     document.getElementById(tabname).classList.add("active-tab");
+    
+}
